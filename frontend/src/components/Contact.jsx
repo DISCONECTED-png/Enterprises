b33c31e2f0c4e7cde56b1f64d270ae357a6dadc5
DISCONECTED-png <anantmoti05@gmail.com>
2025-08-15 17:15:28 +0530

    Update Contact.jsx

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -24,7 +24,7 @@ export const Contact = () => {
     e.preventDefault();
     setButtonText("Sending...");
     try {
-      const response = await fetch("http://localhost:5000/contact", {
+      const response = await fetch("https://enterprises-phi.vercel.app/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json;charset=utf-8",
